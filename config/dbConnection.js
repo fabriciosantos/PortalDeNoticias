32fe50abf0c22169a9ddf369fb2199c4c4118286
Fabricio Santos Guimarães <fabricioashua@gmail.com>
2017-09-18 09:29:20 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,9 +4,9 @@ var connMySql = function () {
     console.log('Requisição para o Banco de dados realizada com sucesso.');
     return mysql.createConnection({
         host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'portal_noticias'
+        user: 'b054ae80c3042a',
+        password: '7f8b5755',
+        database: 'heroku_5caa249a6021fe8'
     });
 };
 
